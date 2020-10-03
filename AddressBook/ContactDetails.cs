08a97fdc13a798e0d620af93fbcaf6f72d7f47c6
sharyu raut <sharyukraut210298@gmail.com>
2020-10-03 09:49:16 +0530

    [Sharyu] Add. Added new contact details

--- AddressBook/ContactDetails.cs
+++ AddressBook/ContactDetails.cs
@@ -6,7 +6,8 @@ namespace AddressBook
 {
     class ContactDetails
     {
-        
+        List<ContactDetails> contacts = new List<ContactDetails>();
+
         //get and set accessor for each contact details field
         public string firstName { get; set; }
         public string lastName { get; set; }
@@ -31,9 +32,43 @@ namespace AddressBook
 
         }
 
+        public ContactDetails()
+        {
+        }
+
         public void display()
         {
-            Console.WriteLine(firstName + " " + lastName + " " + address + " " + city + " " + state + " " + zipCode + " " + phoneNumber + " " + email);
+            Console.WriteLine("\nDisplaying Contacts - \nFirst Name | Last Name | Address |  City  | State | ZIP Code | Phone Number | EmailId");
+            foreach (ContactDetails contact in contacts)
+            {
+                Console.WriteLine(contact.firstName + "\t" + contact.lastName + "\t" + contact.address + "\t" + contact.city + "\t" + contact.state + "\t" + contact.zipCode + "\t\t" + contact.phoneNumber + "\t" + contact.email);
+            }
         }
+
+        public void addNewContact()
+        {
+            ContactDetails contact = new ContactDetails();
+            Console.Write("Enter First Name: ");
+            contact.firstName = Console.ReadLine();
+            Console.Write("Enter Last Name: ");
+            contact.lastName = Console.ReadLine();
+            Console.Write("Enter Address:");
+            contact.address = Console.ReadLine();
+            Console.Write("Enter City: ");
+            contact.city = Console.ReadLine();
+            Console.Write("Enter State: ");
+            contact.state = Console.ReadLine();
+            Console.Write("Enter ZIP Code: ");
+            contact.zipCode = int.Parse(Console.ReadLine());
+            Console.Write("Enter Phone Number: ");
+            contact.phoneNumber = long.Parse(Console.ReadLine());
+            Console.Write("Enter Email Id: ");
+            contact.email = Console.ReadLine();
+
+            contacts.Add(contact);
+            Console.WriteLine("New Contact added successfully");
+        }
+
+        
     }
 }
